34d131fcc6193ebb18e15da4c359220d006da096
Luiz R Bortolotti Junior <luiz_bortolotti@hotmail.com>
2025-11-14 13:37:15 -0300

    feat. ProductService class implementation

---
+++ src/main/java/com/example/springboot_first_project/service/ProductService.java
@@ -0,0 +1,34 @@
+package com.example.springboot_first_project.service;
+
+import com.example.springboot_first_project.model.Product;
+import com.example.springboot_first_project.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> getProduct(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+}
