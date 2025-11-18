5769282f366a39e05bc4e8a9d0c64486843fa147
Luiz R Bortolotti Junior <luiz_bortolotti@hotmail.com>
2025-11-18 20:12:24 -0300

    removing external src folder and other files

--- src/main/java/com/example/springboot_first_project/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.springboot_first_project.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/springboot_first_project/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springboot_first_project.repository;
-
-import com.example.springboot_first_project.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
