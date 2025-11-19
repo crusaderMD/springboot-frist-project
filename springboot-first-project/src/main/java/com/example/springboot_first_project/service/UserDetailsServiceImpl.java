dd2b51c3c7e084502096cf5801e31c59b9de1102
Luiz R Bortolotti Junior <luiz_bortolotti@hotmail.com>
2025-11-19 09:05:52 -0300

    feat SecurityConfig and UserDetailsServiceImpl classes

---
+++ springboot-first-project/src/main/java/com/example/springboot_first_project/service/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.example.springboot_first_project.service;
+
+import com.example.springboot_first_project.model.User;
+import com.example.springboot_first_project.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found!"));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUserName())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
